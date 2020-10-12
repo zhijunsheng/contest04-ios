840d6edf7b44f9e57abcbf4864707888a7bf5201
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2020-10-12 21:40:38 +0800

    We create a new project.

---
+++ Contest04/Contest04/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contest04
+//
+//  Created by Lambert Lin on 2020/10/12.
+//  Copyright © 2020 Lambert Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
