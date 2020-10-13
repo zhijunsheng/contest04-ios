4a0285b62d9f70d11d89bd2fea02fb1936786243
Andy Yan <andy@Andys-MacBook-Air.local>
2020-10-13 16:48:26 -0400

    we created a new project in xcode

---
+++ Contest4/Contest4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contest4
+//
+//  Created by Andy Yan on 2020-10-13.
+//  Copyright © 2020 Andy Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
