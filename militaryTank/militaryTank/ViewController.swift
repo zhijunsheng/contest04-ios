ef6588103ef9ebfacaae77d147facc3471398f21
Elaine <elainexu392@gmail.com>
2020-10-13 19:31:37 -0400

    Made the project.

---
+++ militaryTank/militaryTank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  militaryTank
+//
+//  Created by Elaine on 2020-10-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
