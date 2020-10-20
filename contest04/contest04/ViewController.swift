69124124aea5f7e276db081a55b22e500f5da4f2
daobao <lucashongyuanzhang@gmail.com>
2020-10-20 20:03:57 +0800

    the tank can move

--- contest04/contest04/ViewController.swift
+++ contest04/contest04/ViewController.swift
@@ -1,20 +1,44 @@
-//
-//  ViewController.swift
-//  contest04
-//
-//  Created by daobao on 2020/10/13.
-//  Copyright © 2020 daobao. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tankView: TankView!
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
     }
 
-
+    @IBAction func stop(_ sender: UIButton) {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    @IBAction func go(_ sender: UIButton) {
+        timer?.invalidate()
+        timer = nil
+        timer = Timer.scheduledTimer(withTimeInterval: 1/30, repeats: true) { (_) in
+            self.tankView.dtaX += 1
+            self.tankView.setNeedsDisplay()
+        }
+    }
+    
+    @IBAction func move(_ sender: UISlider) {
+        timer?.invalidate()
+        timer = nil
+        tankView.dtaX = CGFloat(sender.value)
+        tankView.setNeedsDisplay()
+    }
 }
 
+/*
+ 
+ N: 1, 65, 234568
+ Z: -234567899, -1, 0, 1, 2, 3, 4, 5...,
+ Q: 6, 220, 33.4, 38939.38503, 1/7
+ R: π, √5, e = 2.718281828...
+ C: i,
+ 
+ 
+ */
