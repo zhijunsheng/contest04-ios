92057090291c13afada2151102c0c2d783c2c88e
Angie Yan <angieyan@Angies-MacBook-Pro.local>
2020-10-22 16:49:44 -0400

    I opened a new project called "Contest04".

---
+++ Contest04/Contest04/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contest04
+//
+//  Created by Angie Yan on 2020-10-22.
+//  Copyright © 2020 Angie Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
