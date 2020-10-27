97b98473ee48cf4ba01a8070788d078865072d99
Andy Yan <andy@Andys-MacBook-Air.local>
2020-10-27 16:31:10 -0400

    we created the bottom of our tank

--- Contest4/Contest4/BoardView.swift
+++ Contest4/Contest4/BoardView.swift
@@ -11,45 +11,80 @@ import UIKit
 class BoardView: UIView {
     
     override func draw(_ rect: CGRect) {
-        
         drawTires()
         drawRoundTrack()
+        drawTankPart()
         
-        let pencil = UIBezierPath()
-        
-        pencil.move(to: CGPoint(x: 100, y: 100))
-        pencil.addLine(to: CGPoint(x: 300, y: 100))
-        
-        pencil.stroke()
-
     }
     
     func drawRoundTrack() {
-        let roundpart1 = UIBezierPath(arcCenter: CGPoint(x: 100, y: 400), radius: 25, startAngle: CGFloat.pi * 1.5, endAngle: CGFloat.pi * 0.5, clockwise: false)
+        let roundpart3 = UIBezierPath(arcCenter: CGPoint(x: 75, y: 340), radius: 35, startAngle: CGFloat.pi * 0, endAngle: CGFloat.pi * 2, clockwise: false)
         
-        roundpart1.stroke()
+        roundpart3.stroke()
         
-        let roundpart2 = UIBezierPath(arcCenter: CGPoint(x: 400, y: 400), radius: 25, startAngle: CGFloat.pi * 1.5, endAngle: CGFloat.pi * 0.5, clockwise: true)
+        let roundpart4 = UIBezierPath(arcCenter: CGPoint(x: 625, y: 340), radius: 35, startAngle: CGFloat.pi * 0, endAngle: CGFloat.pi * 2, clockwise: true)
         
-        roundpart2.stroke()
+        roundpart4.stroke()
+        
+        for i in 0 ... 2 {
+            let roadwheel = UIBezierPath(arcCenter: CGPoint(x: 125 + 75 * i, y: 400), radius: 25, startAngle: CGFloat.pi * 0, endAngle: CGFloat.pi * 2, clockwise: true)
+            
+            roadwheel.stroke()
+        }
+        
+        for i in 0 ... 2 {
+            let roadwheel = UIBezierPath(arcCenter: CGPoint(x: 425 + 75 * i, y: 400), radius: 25, startAngle: CGFloat.pi * 0, endAngle: CGFloat.pi * 2, clockwise: true)
+            
+            roadwheel.stroke()
+        }
     }
     
     func drawTires() {
-        let wheels = UIBezierPath(arcCenter: CGPoint(x: 100, y: 400), radius: 25, startAngle: CGFloat.pi * 1.5, endAngle: CGFloat.pi * 0.5, clockwise: false)
-        
-        wheels.stroke()
         
         let wheelPart = UIBezierPath()
         
-        wheelPart.move(to: CGPoint(x: 100, y: 375))
-        wheelPart.addLine(to: CGPoint(x: 400, y: 375))
+        wheelPart.move(to: CGPoint(x: 110, y: 375))
+        wheelPart.addLine(to: CGPoint(x: 590, y: 375))
         
-        wheelPart.move(to: CGPoint(x: 100, y: 425))
-        wheelPart.addLine(to: CGPoint(x: 400, y: 425))
+        wheelPart.move(to: CGPoint(x: 125, y: 425))
+        wheelPart.addLine(to: CGPoint(x: 125 + 75 * 6, y: 425))
+        
+        wheelPart.move(to: CGPoint(x: 113, y: 422.5))
+        wheelPart.addLine(to: CGPoint(x: 50, y: 365))
+
+        wheelPart.move(to: CGPoint(x: 587.5, y: 422.5))
+        wheelPart.addLine(to: CGPoint(x: 650, y: 365))
         
         wheelPart.stroke()
         
     }
     
+    func drawTankPart() {
+        let part = UIBezierPath()
+
+        part.move(to: CGPoint(x: 110, y: 375))
+        part.addLine(to: CGPoint(x: 110, y: 325))
+
+        part.move(to: CGPoint(x: 590, y: 375))
+        part.addLine(to: CGPoint(x: 590, y: 325))
+
+        part.move(to: CGPoint(x: 60, y: 325))
+        part.addLine(to: CGPoint(x: 640, y: 325))
+        
+        part.stroke()
+        
+          #colorLiteral(red: 0.352917254, green: 0.785050571, blue: 0.981100738, alpha: 1).setFill()
+        
+        let part2 = UIBezierPath(rect: CGRect(x: 40, y: 275, width: 620, height: 50))
+        
+        part2.fill()
+        part2.stroke()
+        
+        UIBezierPath(rect: CGRect(x: 40, y: 275, width: 100, height: 50)).stroke()
+        UIBezierPath(rect: CGRect(x: 140, y: 275, width: 100, height: 50)).stroke()
+        UIBezierPath(rect: CGRect(x: 240, y: 275, width: 100, height: 50)).stroke()
+        UIBezierPath(rect: CGRect(x: 340, y: 275, width: 100, height: 50)).stroke()
+        UIBezierPath(rect: CGRect(x: 440, y: 275, width: 100, height: 50)).stroke()
+    }
 
 }
