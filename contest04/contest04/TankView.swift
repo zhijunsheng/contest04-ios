4391cfde92ae2e954e6fb6e4a1fca582572d81c5
daobao <lucashongyuanzhang@gmail.com>
2020-11-04 19:33:37 +0800

    we had an "Can Shot Bullet" (tank)

--- contest04/contest04/TankView.swift
+++ contest04/contest04/TankView.swift
@@ -1,92 +1,98 @@
 import UIKit
 
 class TankView: UIView {
-    var dtaX: CGFloat = -200
+    var dtatnkX: CGFloat = -200
+    var dtabltX: CGFloat = 10
     
-    
-   
     override func draw(_ rect: CGRect) {
         // Drawing code
+        
+        drwBullet()
         drwtnkBoard()
     }
 
-    
+    func drwBullet() {
+        
+        
+        let blt: UIImage? = UIImage(named: "bulletboom")
+        blt?.draw(in: CGRect(x: 512.5 + dtabltX + dtatnkX, y: 295.25, width: 50, height: 18.75))
+    }
     
     func drwtnkBoard() {
         #colorLiteral(red: 0.6666666667, green: 0.4751212597, blue: 0.2586010993, alpha: 0.5).setFill()
         let welMark1 = UIBezierPath()
-        welMark1.addArc(withCenter: CGPoint(x: 264 + dtaX, y: 414), radius: 50, startAngle: (dtaX + 200) / 50, endAngle: 0.5 * CGFloat.pi + (dtaX + 200) / 50, clockwise: true)
+        welMark1.addArc(withCenter: CGPoint(x: 264 + dtatnkX, y: 414), radius: 50, startAngle: (dtatnkX + 200) / 50, endAngle: 0.5 * CGFloat.pi + (dtatnkX + 200) / 50, clockwise: true)
         welMark1.fill()
         
         let welMark2 = UIBezierPath()
-        welMark2.addArc(withCenter: CGPoint(x: 464 + dtaX, y: 414), radius: 50, startAngle: (dtaX + 200) / 50, endAngle: 0.5 * CGFloat.pi + (dtaX + 200) / 50, clockwise: true)
+        welMark2.addArc(withCenter: CGPoint(x: 464 + dtatnkX, y: 414), radius: 50, startAngle: (dtatnkX + 200) / 50, endAngle: 0.5 * CGFloat.pi + (dtatnkX + 200) / 50, clockwise: true)
         welMark2.fill()
         
         let welMark3 = UIBezierPath()
-        welMark3.addArc(withCenter: CGPoint(x: 339 + dtaX, y: 439), radius: 25, startAngle: (dtaX + 200) / 25, endAngle: 0.5 * CGFloat.pi + (dtaX + 200) / 25, clockwise: true)
+        welMark3.addArc(withCenter: CGPoint(x: 339 + dtatnkX, y: 439), radius: 25, startAngle: (dtatnkX + 200) / 25, endAngle: 0.5 * CGFloat.pi + (dtatnkX + 200) / 25, clockwise: true)
         welMark3.fill()
         
         let welMark4 = UIBezierPath()
-        welMark4.addArc(withCenter: CGPoint(x: 389 + dtaX, y: 439), radius: 25, startAngle: (dtaX + 200) / 25, endAngle: 0.5 * CGFloat.pi + (dtaX + 200) / 25, clockwise: true)
+        welMark4.addArc(withCenter: CGPoint(x: 389 + dtatnkX, y: 439), radius: 25, startAngle: (dtatnkX + 200) / 25, endAngle: 0.5 * CGFloat.pi + (dtatnkX + 200) / 25, clockwise: true)
         welMark4.fill()
         
         #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5).setStroke()
         let tnkTrack = UIBezierPath()
-        tnkTrack.addArc(withCenter: CGPoint(x: 264 + dtaX, y: 414), radius: 50, startAngle: 1 * CGFloat.pi, endAngle: 0.5 * CGFloat.pi, clockwise: false)
-        tnkTrack.addLine(to: CGPoint(x: 464 + dtaX, y: 464))
-        tnkTrack.addArc(withCenter: CGPoint(x: 464 + dtaX, y: 414), radius: 50, startAngle: 0.5 * CGFloat.pi, endAngle: 0, clockwise: false)
+        tnkTrack.addArc(withCenter: CGPoint(x: 264 + dtatnkX, y: 414), radius: 50, startAngle: 1 * CGFloat.pi, endAngle: 0.5 * CGFloat.pi, clockwise: false)
+        tnkTrack.addLine(to: CGPoint(x: 464 + dtatnkX, y: 464))
+        tnkTrack.addArc(withCenter: CGPoint(x: 464 + dtatnkX, y: 414), radius: 50, startAngle: 0.5 * CGFloat.pi, endAngle: 0, clockwise: false)
         tnkTrack.stroke()
         
         
         
         let tnkLid = UIBezierPath()
         
-        tnkLid.move(to: CGPoint(x: 539 + dtaX, y: 414))
-        tnkLid.addLine(to: CGPoint(x: 189 + dtaX, y: 414))
+        tnkLid.move(to: CGPoint(x: 539 + dtatnkX, y: 414))
+        tnkLid.addLine(to: CGPoint(x: 189 + dtatnkX, y: 414))
         
-        tnkLid.addArc(withCenter: CGPoint(x: 239 + dtaX, y: 414), radius: 50, startAngle: 1 * CGFloat.pi, endAngle: 1.5 * CGFloat.pi, clockwise: true)
-        tnkLid.addArc(withCenter: CGPoint(x: 489 + dtaX, y: 414), radius: 50, startAngle: 1.5 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
+        tnkLid.addArc(withCenter: CGPoint(x: 239 + dtatnkX, y: 414), radius: 50, startAngle: 1 * CGFloat.pi, endAngle: 1.5 * CGFloat.pi, clockwise: true)
+        tnkLid.addArc(withCenter: CGPoint(x: 489 + dtatnkX, y: 414), radius: 50, startAngle: 1.5 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
         #colorLiteral(red: 1, green: 0.9969701477, blue: 0, alpha: 1).setFill()
         tnkLid.fill()
         tnkLid.stroke()
         
         let tnkWheel = UIBezierPath()
-        tnkWheel.addArc(withCenter: CGPoint(x: 264 + dtaX, y: 414), radius: 50, startAngle: 0, endAngle: 0.5 * CGFloat.pi, clockwise: true)
-        tnkWheel.addArc(withCenter: CGPoint(x: 339 + dtaX, y: 439), radius: 25, startAngle: 0.5 * CGFloat.pi, endAngle: 2.5 * CGFloat.pi, clockwise: true)
-        tnkWheel.addArc(withCenter: CGPoint(x: 389 + dtaX, y: 439), radius: 25, startAngle: 0.5 * CGFloat.pi, endAngle: 2.5 * CGFloat.pi, clockwise: true)
-        tnkWheel.addArc(withCenter: CGPoint(x: 464 + dtaX, y: 414), radius: 50, startAngle: 0.5 * CGFloat.pi, endAngle: CGFloat.pi, clockwise: true)
+        tnkWheel.addArc(withCenter: CGPoint(x: 264 + dtatnkX, y: 414), radius: 50, startAngle: 0, endAngle: 0.5 * CGFloat.pi, clockwise: true)
+        tnkWheel.addArc(withCenter: CGPoint(x: 339 + dtatnkX, y: 439), radius: 25, startAngle: 0.5 * CGFloat.pi, endAngle: 2.5 * CGFloat.pi, clockwise: true)
+        tnkWheel.addArc(withCenter: CGPoint(x: 389 + dtatnkX, y: 439), radius: 25, startAngle: 0.5 * CGFloat.pi, endAngle: 2.5 * CGFloat.pi, clockwise: true)
+        tnkWheel.addArc(withCenter: CGPoint(x: 464 + dtatnkX, y: 414), radius: 50, startAngle: 0.5 * CGFloat.pi, endAngle: CGFloat.pi, clockwise: true)
         tnkWheel.stroke()
         
-        let tnkMark = UIBezierPath(rect: CGRect(x: 189 + dtaX, y: 339, width: 25, height: 50))
+        let tnkMark = UIBezierPath(rect: CGRect(x: 189 + dtatnkX, y: 339, width: 25, height: 50))
         #colorLiteral(red: 1, green: 1, blue: 0, alpha: 1).setFill()
         tnkMark.fill()
         tnkMark.stroke()
         
-        let tnkNeck = UIBezierPath(rect: CGRect(x: 239 + dtaX, y: 339, width: 150, height: 25))
+        let tnkNeck = UIBezierPath(rect: CGRect(x: 239 + dtatnkX, y: 339, width: 150, height: 25))
         #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1).setFill()
         tnkNeck.fill()
         tnkNeck.stroke()
         
-        let tnkHead = UIBezierPath(rect: CGRect(x: 226.5 + dtaX, y: 289, width: 175, height: 50))
+        let tnkHead = UIBezierPath(rect: CGRect(x: 226.5 + dtatnkX, y: 289, width: 175, height: 50))
         #colorLiteral(red: 1, green: 0.9969701477, blue: 0, alpha: 1).setFill()
         tnkHead.fill()
         tnkHead.stroke()
         
         let tnkHair = UIBezierPath()
-        tnkHair.move(to: CGPoint(x: 239 + dtaX, y: 289))
-        tnkHair.addLine(to: CGPoint(x: 251.5 + dtaX, y: 264))
-        tnkHair.addLine(to: CGPoint(x: 376.5 + dtaX, y: 264))
-        tnkHair.addLine(to: CGPoint(x: 389 + dtaX, y: 289))
+        tnkHair.move(to: CGPoint(x: 239 + dtatnkX, y: 289))
+        tnkHair.addLine(to: CGPoint(x: 251.5 + dtatnkX, y: 264))
+        tnkHair.addLine(to: CGPoint(x: 376.5 + dtatnkX, y: 264))
+        tnkHair.addLine(to: CGPoint(x: 389 + dtatnkX, y: 289))
         #colorLiteral(red: 1, green: 0.9969701477, blue: 0, alpha: 1).setFill()
         tnkHair.fill()
         tnkHair.stroke()
         
-        let tnkcnoPiece = UIBezierPath(rect: CGRect(x: 401.5 + dtaX, y: 289, width: 25, height: 37.5))
+        let tnkcnoPiece = UIBezierPath(rect: CGRect(x: 401.5 + dtatnkX, y: 289, width: 25, height: 37.5))
         #colorLiteral(red: 1, green: 0.9969701477, blue: 0, alpha: 1).setFill()
         tnkcnoPiece.fill()
         tnkcnoPiece.stroke()
         
-        let tnkCannon = UIBezierPath(rect: CGRect(x: 426.5 + dtaX, y: 295.25, width: 150, height: 18.75))
+        let tnkCannon = UIBezierPath(rect: CGRect(x: 426.5 + dtatnkX, y: 295.25, width: 150, height: 18.75))
         #colorLiteral(red: 1, green: 0.9969701477, blue: 0, alpha: 1).setFill()
         tnkCannon.fill()
         tnkCannon.stroke()
--- contest04/contest04/ViewController.swift
+++ contest04/contest04/ViewController.swift
@@ -3,13 +3,26 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var tankView: TankView!
     var timer: Timer?
+ //   var bomb: UIImage
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
         
     }
-
+    
+    @IBAction func boom(_ sender: UIButton) {
+        Timer.scheduledTimer(withTimeInterval: 1/30, repeats: true) { (t) in
+            self.tankView.dtabltX += 5/3
+            self.tankView.setNeedsDisplay()
+            if self.tankView.dtabltX >= 625 {
+                self.tankView.dtabltX = 10
+                t.invalidate()
+            }
+        }
+    }
+    
     @IBAction func stop(_ sender: UIButton) {
         timer?.invalidate()
         timer = nil
@@ -19,7 +32,7 @@ class ViewController: UIViewController {
         timer?.invalidate()
         timer = nil
         timer = Timer.scheduledTimer(withTimeInterval: 1/30, repeats: true) { (_) in
-            self.tankView.dtaX += 1
+            self.tankView.dtatnkX += 1
             self.tankView.setNeedsDisplay()
         }
     }
@@ -27,7 +40,7 @@ class ViewController: UIViewController {
     @IBAction func move(_ sender: UISlider) {
         timer?.invalidate()
         timer = nil
-        tankView.dtaX = CGFloat(sender.value) - 200
+        tankView.dtatnkX = CGFloat(sender.value) - 200
         tankView.setNeedsDisplay()
     }
 }
@@ -40,5 +53,8 @@ class ViewController: UIViewController {
  R: π, √5, e = 2.718281828...
  C: i,
  
+ 2π radians = 360 degrees
+ radius ~ radian?
+ 2π(...) = 2π(...)
  
  */
