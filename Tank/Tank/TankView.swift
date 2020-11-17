4ace421903b36df8c18c3470816965704658ad84
Corey Gao <coreygao@Coreys-MacBook-Air.local>
2020-11-16 22:00:19 -0500

    drew some lines

--- Tank/Tank/TankView.swift
+++ Tank/Tank/TankView.swift
@@ -10,12 +10,23 @@ import UIKit
 
 class TankView: UIView {
 
-    /*
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+      
+        let pencil = UIBezierPath()
+        pencil.move(to: CGPoint(x: 300, y: 500))
+        pencil.addLine(to: CGPoint(x: 280, y: 400))
+        pencil.addLine(to: CGPoint(x: 380, y: 360))
+       
+        #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).setStroke()
+        pencil.lineWidth = 3
+        pencil.stroke()
+        
+    
+    
     }
-    */
+   
 
 }
