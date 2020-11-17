55b2c6f6f0a2322e9b3060a3044089647e3261c5
Corey Gao <coreygao@Coreys-MacBook-Air.local>
2020-11-16 21:37:17 -0500

    added a file into project tank

---
+++ Tank/Tank/TankView.swift
@@ -0,0 +1,21 @@
+//
+//  TankView.swift
+//  Tank
+//
+//  Created by Corey Gao on 2020-11-16.
+//  Copyright © 2020 Corey Gao. All rights reserved.
+//
+
+import UIKit
+
+class TankView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
